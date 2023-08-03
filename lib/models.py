38cb6810a87a7af4c19f6ef16defa77ab4f26dc3
Kevin Bravo <kbravo1024@gmail.com>
2023-08-02 17:23:16 -0700

    complete many to many lab

--- lib/models.py
+++ lib/models.py
@@ -26,6 +26,8 @@ class Game(Base):
     genre = Column(String())
     platform = Column(String())
     price = Column(Integer())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
 
     reviews = relationship('Review', backref=backref('game'))
 
@@ -42,6 +44,8 @@ class Review(Base):
     id = Column(Integer(), primary_key=True)
     score = Column(Integer())
     comment = Column(String())
+    created_at = Column(DateTime(), server_default=func.now())
+    updated_at = Column(DateTime(), onupdate=func.now())
     
     game_id = Column(Integer(), ForeignKey('games.id'))
 
